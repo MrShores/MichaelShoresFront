88d9f422dafb4a5ae6a1e0d3b4c41a7fa1226eb8
Michael Shores <m@michaelshores.com>
2015-11-10 12:33:57 -0800

    Initial commit, basic styles

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Michael Shores | Designer &amp; Developer</title>
+
+    <!-- Google Fonts -->
+    <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,400italic,700,300italic' rel='stylesheet' type='text/css'>
+
+    <link rel="stylesheet" href="css/style.css" />
+
+</head>
+<body>
+
+    <div class="navigation">
+        <div class="logo">
+            <a href="/"><img src="img/m.png" alt="Michael Shores" /></a>
+        </div>
+        <ul class="nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#">Articles</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+        <div class="copyright">
+            &copy; <?php echo date('Y'); ?> Michael Shores
+        </div>
+    </div>
+
+    <div id="main">
+
+        <div class="header">
+            <div class="title"><div class="text"><span>Michael</span> Shores</div></div>
+        </div>
+
+
+        <div class="article">
+            <h1><span>Michael Shores</span></h1>
+
+            <p class="lead">And as the science of man is the-only solid for the other sciences, so the only solid foundation we can give to this science itself must be laid on experience and observation. It is no astonishing reflection to</p>
+            <p>And as the science of man is the-only solid foundation for the other sciences, so the only solid foundation we can give to this science itself must be laid on experience and observation. It is no astonishing reflection to consider, that the application of experimental philosophy to moral subjects should come after that to natural at the distance of above a whole century; since we find in fact, that there was about the same interval betwixt the origins of these sciences; and that reckoning from <a href="#">THALES to SOCRATES</a>, the space of time is nearly equal to that betwixt, my Lord Bacon and some late philosophers [Mr. Locke, my Lord Shaftesbury, Dr. Mandeville, Mr. Hutchinson, Dr. Butler, etc.] in England, who have begun to put the science of man on a new footing, and have engaged the attention, and excited the curiosity of the public.</p>
+            <p>So true it is, that however other nations may rival us in poetry, and excel us in some other agreeable arts, the improvements in reason and philosophy can only be owing to a land of toleration and of liberty.</p>
+
+
+            <blockquote>
+                <span>So true it is, that however other nations may rival us in poetry, and excel us in some other agreeable arts, the improvements in reason and philosophy can only be owing to a land of toleration and of liberty.</span>
+            </blockquote>
+
+        </div>
+        
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+
+</body>
+</html>
